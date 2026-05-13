184a091057f700ade7ceb011c8b07b920f4b4bd0
eka-gumelar <miracle.khae@gmail.com>
2026-05-13 08:54:07 +0700

    Add files via upload

---
+++ PP/database/data_tubes.js
@@ -0,0 +1,4 @@
+const DATA_TUBES = [
+    { pn: "TUBE-5MM-BLK", loc: "RAK T1", img1: "tube-5mm.png", duration: 5 }, 
+    { pn: "TUBE-7MM-RED", loc: "RAK T2", img1: "tube-7red.png", duration: 7 }
+];
